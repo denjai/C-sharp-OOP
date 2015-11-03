c56e8c9ebf3a4fd1cb09fd238de32260b14db1ec
Doncho Toromanov <doncho.t@gmail.com>
2015-11-03 17:16:26 +0200

    added

---
+++ Extension-Methods-Delegates-Lambda-LINQ/Extension-Methods-Delegates-Lambda-LINQ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Methods_Delegates_Lambda_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
